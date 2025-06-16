d671661c67b0d18355e680d8e152534856b40a60
Ubuntu <ubuntu@ip-172-31-16-19.ec2.internal>
2025-06-16 18:32:43 +0000

    Add gunicorn and nginx

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-# app.py
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, send_from_directory
+
+app = Flask(__name__, static_folder="dist", static_url_path='')
 
 @app.route('/')
-def hello():
-    return "Hello from Flask and Gunicorn!"
+def index():
+    return send_from_directory(app.static_folder, 'index.html')
 
-if __name__ == "__main__":
+@app.route('/<path:path>')
+def static_files(path):
+    return send_from_directory(app.static_folder, path)
+
+@app.route('/api/hello')
+def hello():
+    return {'msg': 'Hello from Flask + Vite!'}
+
+if __name__ == '__main__':
     app.run()
